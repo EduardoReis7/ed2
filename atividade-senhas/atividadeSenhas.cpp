15cf0b9abccebdcaaf2bcd37ee468a4d0267a140
eduardoreis7 <eduardodovigireis@gmail.com>
2023-05-22 20:52:44 -0300

    add atividade senhas

--- atividade-senhas/atividadeSenhas.cpp
+++ atividade-senhas/atividadeSenhas.cpp
@@ -1,40 +1,61 @@
 #include <stdlib.h>
 #include <stdio.h> 
+#define SIZE 456976
+
+void geraSenhaFor(FILE *arqSenha, char i, char j, char k, char l);
+void geraSenhaRecursivo();
 
 int main() {
 	
 	FILE * arqSenha;
 	char i, j, k, l;
+	int m, opt;
 	
 	if ((arqSenha = fopen("Arquivo.txt", "w+")) == NULL){
 		printf("Erro ao abrir arquivo.");
 	} else {
-	
-		i = 65;
+		printf("============================\n");
+		printf("==  GERADOR DE SENHAS     ==\n");
+		printf("============================\n");
+		printf("Escolha a forma de gerar a senha:\n");
+		printf("[1] Gerar senha com for\n");
+		printf("[2] Gerar senha recursivamente\n");
+		scanf("%d", &opt);
 		
-		while (i <= 90) {
-			j = 65;
-			k = 65;
-			l = 65;
-			fprintf(arqSenha, "\n%c %c %c %c", i, j, k, l);
-			while (j <= 90) {
-				k = 65;
-				l = 65;
-				fprintf(arqSenha, "\n%c %c %c %c", i, j, k, l);
-				while (k <= 90) {
-					l = 65;
-					fprintf(arqSenha, "\n%c %c %c %c", i, j, k, l);
-					while (l <= 90) {
-						fprintf(arqSenha, "\n%c %c %c %c", i, j, k, l);
-						l++;
-					}
-					k++;
-				}
-				j++;
-			}
-			i++;
+		switch(opt) {
+			case 1:
+				geraSenhaFor(arqSenha, i, j, k, l);
+				break;
+			case 2:
+				//geraSenhaRecursivo(arqSenha, SIZE, i, j, k, l);
+				break;
+			default:
+				printf("Opção inválida!\n");
 		}
+		printf("\nArquivo gerado com sucesso, verifique seu diretorio!");
 		fclose(arqSenha);
 	}
-	
 }
+
+void geraSenhaFor(FILE *arqSenha, char i, char j, char k, char l) {
+	int m = 0;
+	while (m < SIZE) {
+		for (i = 65; i <= 90; i++) {
+			for (j = 65; j <= 90; j++) {
+				for (k = 65; k <= 90; k++) {
+					for (l = 65; l <= 90; l++) {
+						fprintf(arqSenha, "%c%c%c%c\n", i, j, k, l);
+						m++;
+					}
+				}
+			}
+		}
+	}
+}
+
+/*void geraSenhaRecursivo(FILE *arqSenha, int tamanho, char *i, char *j, char *k, char *l) {
+	if (tamanho == 0) {
+		printf("Terminou!\n");
+	}
+	geraSenhaRecursivo(arqSenha, tamanho--, i, j, k, l)
+}*/
